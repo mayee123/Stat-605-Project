769a6be2f0abe99e4f06df9daabda5d3374a6f84
mayee123 <101478879+mayee123@users.noreply.github.com>
2024-12-16 10:49:39 -0800

    Add files via upload

---
+++ data_wrangling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+#read in csv files
+df1 = pd.read_csv('typing.csv')
+df2 = pd.read_csv('nuccore.csv')
+df3=pd.read_csv('amr.tsv',sep='\t')
+
+#See number of each observed host range taxonomic rank
+unique_values = df1['observed_host_range_ncbi_rank'].value_counts()
+print(unique_values)
+
+#create new dataframe called df_amr with the number of Antimicrobial Resistance Genes
+amr=df3['NUCCORE_ACC'].value_counts()
+df_amr=(pd.DataFrame({'NUCCORE_ACC':amr.index, 'AMR_COUNT':amr.values}))
+
+#merge the dataframes on the 'NUCCORE_ACC' row, filling in 0 for AMR_COUNT if the accession does not have any Antimicrobial Resistance Genes
+merged_df1 = pd.merge(df1, df2, on='NUCCORE_ACC')
+merged_df2 = pd.merge(merged_df1,df_amr, on='NUCCORE_ACC',how='left')
+merged_df2['AMR_COUNT'] = merged_df2['AMR_COUNT'].fillna(0)
+
+#filter out any accessions without 'observed_host_range_ncbi_rank'
+df_filtered = merged_df2[merged_df2['observed_host_range_ncbi_rank'].notnull()]
+
+#create new column 'range' with value 0 if observed_host_range_ncbi_rank is 'genus' and 1 otherwise
+df_filtered['range'] = df_filtered['observed_host_range_ncbi_rank'].apply(
+    lambda x: 0 if x == 'genus' else 1)
+
+#create csv file called 'plasmid_data.csv'
+df_filtered.to_csv('plasmid_data.csv', index=False)
+
